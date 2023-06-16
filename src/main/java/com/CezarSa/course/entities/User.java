fa20c30f1153aed331a144cb1d218cbe20d545fc
Cezar Von Held <cezarsa9@gmail.com>
2023-06-16 12:03:36 -0300

    order, instant, ISO 8601 - part 1

--- src/main/java/com/CezarSa/course/entities/User.java
+++ src/main/java/com/CezarSa/course/entities/User.java
@@ -1,12 +1,16 @@
 package com.CezarSa.course.entities;
 
+
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -23,8 +27,11 @@ public class User implements Serializable{
 	private String phone;
 	private String password;
 	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
+	
 	public User() {
-		super();
 	}
 
 	public User(Long id, String name, String email, String phone, String password) {
@@ -74,7 +81,11 @@ public class User implements Serializable{
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -91,6 +102,10 @@ public class User implements Serializable{
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", email=" + email + ", phone=" + phone + ", password=" + password
+				+ "]";
+	}	
 }
