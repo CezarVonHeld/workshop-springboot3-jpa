78d6ba9d156f395269f6a041aee52670ac8d8e3e
Cezar Von Held <cezarsa9@gmail.com>
2023-06-17 15:25:50 -0300

    Exception handling - update

--- src/main/java/com/CezarSa/course/services/UserService.java
+++ src/main/java/com/CezarSa/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.CezarSa.course.repositories.UserRepository;
 import com.CezarSa.course.resources.exceptions.DatabaseException;
 import com.CezarSa.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -45,9 +47,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
