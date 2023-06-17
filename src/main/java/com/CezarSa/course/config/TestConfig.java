cd4b2ecf2bc110801448f578aca8f952a4615fb9
Cezar Von Held <cezarsa9@gmail.com>
2023-06-17 11:43:51 -0300

    Payment, one-to-one association

--- src/main/java/com/CezarSa/course/config/TestConfig.java
+++ src/main/java/com/CezarSa/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.CezarSa.course.entities.Category;
 import com.CezarSa.course.entities.Order;
 import com.CezarSa.course.entities.OrderItem;
+import com.CezarSa.course.entities.Payment;
 import com.CezarSa.course.entities.Product;
 import com.CezarSa.course.entities.User;
 import com.CezarSa.course.entities.enums.OrderStatus;
@@ -81,6 +82,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
-
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
